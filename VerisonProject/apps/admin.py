54ebbf88c21dc0666df4287df3fd51a94f0e7912
imsanjeev1 <kumar.sanjeev84@gmail.com>
2023-12-01 20:49:12 +0530

    #3 modified admin page according to import export library

--- VerisonProject/apps/admin.py
+++ VerisonProject/apps/admin.py
@@ -1,53 +1,61 @@
 from django.contrib import admin
-
+from django.contrib.admin import widgets
+from .models import Employees
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import EmployeeList
-from django import forms
+from django.contrib import messages
 from django.urls import path
 from django.shortcuts import render
-import csv,io
-from django.contrib import messages
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django import forms
+class EmpResource(resources.ModelResource):
+    class Meta:
+        model = Employees
+        import_id_fields = ('name',)
+        # fields = ('name', 'title')
 
+
+# Register your models here.
 class FileUpload(forms.Form):
-    csv_upload=forms.FileField()
+    view_on_site = False
+    site_url = None
+    select_csv_File=forms.FileField()
 
+class EmployeeAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = EmpResource
 
-class csvfiledataAdmin(admin.ModelAdmin):
-    list_display = ('UniqueIdentifier','Name','ReportsTo','Designation')
+    search_fields = ['uniqueIdentifier','name','reportsTo','designation','address','organisationName','remarks']
+    list_display = ['uniqueIdentifier','name','reportsTo','designation','address','organisationName','remarks','status']
+
+    fields = ['uniqueIdentifier', 'name', 'reportsTo', 'designation', 'address', 'organisationName', 'remarks',
+              'status']
+    list_per_page = 10
+    list_max_show_all = 6
+
+    actions = ['activate', 'deactivate']
+
+    def activate(self, request, queryset):
+        queryset.update(status=True)
+        messages.success(request, "User successfully Activated.")
+
+    def deactivate(self, request, queryset):
+        queryset.update(status=False)
+        messages.warning(request, "User successfully deactivated.")
 
     def get_urls(self):
         urls=super().get_urls()
+        # print("GET URLS>>>",urls)
         new_urls=[path('upload-csv/',self.upload_csv),]
         return new_urls + urls
 
     def upload_csv(self,request):
-        if request.method=="POST":
-            print("Action >>>POST")
-            csv_file=request.FILES["csv_upload"]
-            if not csv_file.name.endswith('.csv'):
-                messages.warning(request,'File type not supported')
-                return HttpResponseRedirect(request.path_info)
-            data_read = csv_file.read().decode('UTF-8')
-            # file_data =data_read.split("\n")
-            io_string = io.StringIO(data_read)
-            next(io_string)
-            for column in csv.reader(io_string,delimiter=',',quotechar="|"):
-                created = EmployeeList.objects.update_or_create(
-                    UniqueIdentifier=column[0],
-                    Name=column[1],
-                    ReportsTo=column[2],
-                    Designation=column[3]
-
-                )
-            url=reverse('admin:index')
-            return HttpResponseRedirect(url)
-
         form = FileUpload()
-        upload_form = {'form':form}
-        return render(request,"admin/csv_upload.html",upload_form)
+        upload_form = {'form': form}
+        return render(request, "admin/csv_upload.html",upload_form)
+    #
 
-
-admin.site.register(EmployeeList,csvfiledataAdmin)
-#Register your models here.
+admin.site.register(Employees,EmployeeAdmin)
+# class Media:
+#     js = (
+#         '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',  # jquery
+#         # 'js/admin.js',  # project static folder
+#     )
